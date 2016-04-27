d7d1fee13c279c53c99d81fcde2313388d9de919
NastassiaVasilets <Nastena_vg@.mail.ru>
2016-04-27 16:15:54 +0300

    create form

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	app = express(),
 	homeControler = require('./src/controllers/home.js'),
 	helloController = require('./src/controllers/hello.js'),
+	servicesController = require('./src/controllers/services.js'),
 	serviceController = require('./src/controllers/service.js'),
 	mongoose = require('mongoose');
 //TODO: Избавиться от хардкода(сделать конфиг) 
@@ -11,12 +12,23 @@ mongoose.connect('mongodb://localhost/lenka');
 app.set('view engine', 'jade');
 app.set('views', './src/views');
 app.get('/', homeControler);
+app.get('/services', servicesController);
 app.get('/services/:id', serviceController);
 app.get('/hello', helloController);
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+   // Prepare output in JSON format
+   response = {
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
 //already last(error processing)
 app.use(function(req, res) {
   res.render('404.jade');
-});
 
 var server = app.listen(3000, function() {
 	console.log('Working 3000');
