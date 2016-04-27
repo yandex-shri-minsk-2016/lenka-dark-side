0c848cd7854b0f634dcc6cbe9582a1797360088e
NastassiaVasilets <Nastena_vg@.mail.ru>
2016-04-27 18:43:31 +0300

    correct bag with service controller

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var express = require('express'),
 	helloController = require('./src/controllers/hello.js'),
 	servicesController = require('./src/controllers/services.js'),
 	serviceController = require('./src/controllers/service.js'),
+	formController = require('./src/controllers/form.js');
 	mongoose = require('mongoose');
+	bodyParser = require('body-parser');
+
 //TODO: Избавиться от хардкода(сделать конфиг) 
 mongoose.connect('mongodb://localhost/lenka');
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 // app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', './src/views');
@@ -15,21 +21,15 @@ app.get('/', homeControler);
 app.get('/services', servicesController);
 app.get('/services/:id', serviceController);
 app.get('/hello', helloController);
-app.post('/process_post', urlencodedParser, function (req, res) {
-
-   // Prepare output in JSON format
-   response = {
-       first_name:req.body.first_name,
-       last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+app.use('/serviceChoose', bodyParser.urlencoded({
+    extended: true
+}));
+app.post('/serviceChoose', function(req, res, next){
+    console.log(req.body);
 });
-
 //already last(error processing)
 app.use(function(req, res) {
   res.render('404.jade');
-
 var server = app.listen(3000, function() {
 	console.log('Working 3000');
 });
--- src/controllers/service.js
+++ src/controllers/service.js
@@ -2,6 +2,6 @@ var ServiceModel = require('../models/service').model;
 
 module.exports = function ServiceController(req, res, next) {
     ServiceModel.findOne({_id: req.params.id}, function(err, service){
-        res.render('menuPage', service || {});
+        res.render('menuPage', {dishes: service.dishes} || {});
     });
 }
