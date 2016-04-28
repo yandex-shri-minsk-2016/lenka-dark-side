37f910aa8d2a45265b6f1afd944b332f3c144cb5
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-04-28 08:23:32 +0300

    make config for mongo

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express'),
-	app = express(),
-	homeControler = require('./src/controllers/home.js'),
-	helloController = require('./src/controllers/hello.js'),
-	serviceController = require('./src/controllers/service.js'),
-	mongoose = require('mongoose');
-//TODO: Избавиться от хардкода(сделать конфиг) 
-mongoose.connect('mongodb://localhost/lenka');
+    app = express(),
+    homeController = require('./src/controllers/home.js'),
+    helloController = require('./src/controllers/hello.js'),
+    serviceController = require('./src/controllers/service.js'),
+    config = require('./src/config/config.js'),
+    mongoose = require('mongoose');
+
+// Connect to mongodb
+var connect = function () {
+    var options = {server: {socketOptions: {keepAlive: 1}}};
+    mongoose.connect(config.db, options);
+};
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
 
 // app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', './src/views');
-app.get('/', homeControler);
+app.get('/', homeController);
 app.get('/services/:id', serviceController);
 app.get('/hello', helloController);
 
-var server = app.listen(3000, function() {
-	console.log('Working 3000');
+var server = app.listen(3000, function () {
+    console.log('Working 3000');
 });
