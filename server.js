e8c01c04c5e25bc6b61113914da99ce426966205
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-04-28 20:54:32 +0300

    new config

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ var express = require('express'),
     homeController = require('./src/controllers/home.js'),
     helloController = require('./src/controllers/hello.js'),
     serviceController = require('./src/controllers/service.js'),
-    config = require('./src/config/config.js'),
+    config = require('config'),
     mongoose = require('mongoose');
 
-// Connect to mongodb
-var connect = function () {
-    var options = {server: {socketOptions: {keepAlive: 1}}};
-    mongoose.connect(config.db, options);
-};
-connect();
-
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
+    mongoose.connect(config.get('mongoose.uri'));
 
 // app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
