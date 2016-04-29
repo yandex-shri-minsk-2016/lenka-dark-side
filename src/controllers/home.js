48aebe7cb37df16496cbe2c70121bc6fa60cdc52
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-04-29 12:29:44 +0300

    error handling homeController

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,6 +2,7 @@ var OrderModel = require('../models/order').model;
 
 module.exports = function HomeController(req, res) {
     OrderModel.find({}, function (err, orders) {
-        res.render('index', {orders: orders});
+        if (err) res.render('404');
+        else res.render('index', {orders: orders});
     });
 };
