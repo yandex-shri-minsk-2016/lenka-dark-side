19dc5c1591d3db39f9eb0fcf2aad45db12f9c32e
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-04-29 12:26:44 +0300

    error handling

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,7 +1,8 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function ServiceController(req, res) {
-    ServiceModel.findOne({_id: req.params.id}, function (err, service) {
-        res.render('menuPage', service || {});
+    ServiceModel.findOne(req.params.id, function (err, service) {
+        if (err) res.render('404');
+        else res.render('menuPage', service || {});
     });
 };
