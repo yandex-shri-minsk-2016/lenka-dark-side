e8d3ccae3f57b5c0dfbe93c0a478a64979d81448
zelenkoff <skyydail@gmail.com>
2016-04-30 19:08:23 +0300

    companypage-frontend go to menupage in to backend

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,13 +1,11 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function ServiceController(req, res, next) {
-    ServiceModel.findOne({_id: req.body.serviceId}, function(err, service) {
+    ServiceModel.findOne({_id: req.params.id}, function(err, service){
         if (err) {
             return next(err);
         }
-        
         var orderInfo = {time: req.body.orderTime, 'service': service};
-        
-        res.render('menuPage', {'orderInfo': orderInfo});
+        res.render('menuPage', {'orderInfo': orderInfo, service: service});
     });
 };
