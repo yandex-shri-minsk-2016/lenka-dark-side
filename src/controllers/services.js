01afd86ab2f807281e752add4b586bdef09656c7
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-05-04 13:37:51 +0300

    small changed in services

--- src/controllers/services.js
+++ src/controllers/services.js
@@ -1,7 +1,7 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function ServicesController(req, res) {
-    ServiceModel.find({}).exec(function(err, services) {
+    ServiceModel.find({}, function(err, services) {
         if (err) {
             return next(err);
         }
