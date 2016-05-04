c36541801fc6b5b201575a94e4b288b89338f890
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-05-04 11:16:33 +0300

    delete consolelog

--- src/controllers/services.js
+++ src/controllers/services.js
@@ -1,11 +1,10 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function ServicesController(req, res) {
-    ServiceModel.find({}).populate('dishes').exec(function(err, services) {
+    ServiceModel.find({}).exec(function(err, services) {
         if (err) {
             return next(err);
         }
-        console.log(services);
         res.render('services', {services: services});
     });
 };
