43248fb9ab981273ca53adbb7b8d8288e6981da2
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-05-08 16:19:30 +0300

    added list subscribers, added field "dishes" to order,

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,11 +1,13 @@
 var OrderModel = require('../models/order').model;
 
-module.exports = function HomeController(req, res, next) {
-    OrderModel.find({}).populate("dishes").exec(function(err, orders) {
+module.exports = function HomeController(req, res, next) {    
+    OrderModel.find({})
+        .populate('dishes')
+        .populate('subscriber.dishes')
+        .exec(function(err, orders) {
         if (err) {
             return next(err);
         }
-
         res.render('index', {orders: orders, user: req.user});
     });
 };
