4c293ae3c7f858f2a4af051b2b6e2df5db2304ca
AlexBazhin <alexbazhyn@gmail.com>
2016-05-08 16:55:45 +0300

    add timer for total

---
+++ src/blocks/total/total.js
@@ -0,0 +1,18 @@
+var timeStart = new Date().getMilliseconds();
+
+
+function totalDisplay() {
+    document.getElementById('total').style.display='block';
+}
+function timer(time) {
+    var timeFinishHours = + (time[0]+time[1]);
+    var timeFinishMinutes = + (time[3]+time[4]);
+
+    timeFinishHours*=3600000;
+    timeFinishMinutes*=60000;
+    var timer = timeFinishHours+timeFinishMinutes-timeStart;
+    alert(timer);
+    //setTimeout(func, timeFinish-timeStart);    
+}
+
+
