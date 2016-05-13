8476140b4defc1749e8426183166e0d5944aea63
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-13 17:31:55 +0300

    push _id og dish instead dish.name dish.price

--- src/controllers/addOrder.js
+++ src/controllers/addOrder.js
@@ -4,30 +4,20 @@ var Promise = require('promise');
 module.exports = function(req, res) {
     if (req.body.action == 'add') {
         var item;
-        var promise = new Promise(function(resolve, reject) {
-            if (!req.session.dishes) {
+        if (!req.session.dishes) {
                 req.session.dishes = [];
+        }
+        item = {};
+        DishModel.findOne({_id: req.body.dishId}, function(err, dish){
+            if (err) {
+                return next(err);
             }
-            item = {};
+            console.log("dish"+dish);
+            item = dish;
+            console.log("item"+item);
         });
-        promise.then(function(){
-            DishModel.findOne({_id: req.body.dishId}, function(err, dish){
-                if (err) {
-                    return next(err);
-                }
-                console.log("dish"+dish);
-                item = dish;
-                console.log("item"+item);
-            });
-        });
-        promise.then(function(){
-            console.log("item after" + item);
-            req.session.dishes = req.session.dishes.concat(item);
-            res.redirect('back');
-        });
-        
-        //item.title = req.body.dishTitle;
-        //item.price = req.body.dishPrice;
-        
+        console.log("item after" + item);
+        req.session.dishes = req.session.dishes.concat(item);
+        res.redirect('back');
     }
-}
+};
