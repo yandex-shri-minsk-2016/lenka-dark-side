b1eef3104159a94a237e429fda00c084f23057bd
AlexBazhin <alexbazhyn@gmail.com>
2016-05-13 21:30:55 +0300

    fix time in timer

--- src/blocks/total/total.js
+++ src/blocks/total/total.js
@@ -1,16 +1,24 @@
-// var timeStart = new Date().getMilliseconds();
-//
-// function totalDisplay() {
-//     document.getElementById('total').style.display='block';
-// }
-// function timer(time) {
-//     var timeFinishHours = + (time[0]+time[1]);
-//     var timeFinishMinutes = + (time[3]+time[4]);
-//
-//     timeFinishHours*=3600000;
-//     timeFinishMinutes*=60000;
-//     var timer = timeFinishHours+timeFinishMinutes-timeStart;
-//     setTimeout(totalDisplay, timer);
-// }
-//
-//
+var timeStartHours = new Date().getHours();
+var timeStartMinutes = new Date().getMinutes();
+
+function totalDisplay() {
+    document.getElementById('total').style.display='block';
+}
+function timer(time) {
+    var timeFinishHours = + (time[0]+time[1]);
+   
+    var timeFinishMinutes = + (time[3]+time[4]);
+    
+    timeFinishHours*=3600000;
+    timeFinishMinutes*=60000;
+
+    timeStartHours*=3600000;
+    timeStartMinutes*=60000;
+    var timeStart = timeStartHours + timeStartMinutes;
+    var timer = timeFinishHours+timeFinishMinutes-timeStart;      
+    if (timer>0) {
+        setTimeout(totalDisplay, timer);
+    }    
+}
+
+
