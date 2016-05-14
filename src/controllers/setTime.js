11ce7bbda00504c3903689b601f67cbb31ac0bed
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-14 09:32:10 +0300

    make a time validation

--- src/controllers/setTime.js
+++ src/controllers/setTime.js
@@ -2,8 +2,15 @@ var ServiceModel = require('../models/service').model;
 
 module.exports = function setTime(req, res, next) {
     if (req.body.action == 'setTime') {
-        req.session.time = req.body.hrs + ":" + req.body.mnts;
-        req.session.serviceId = req.body.serviceId;
-        res.redirect('/services/'+req.body.serviceId)
+        var hrs = +req.body.hrs;
+        var mnts = +req.body.mnts;
+        if (((hrs ^ 0) === hrs) && ((mnts ^ 0) === mnts) && (hrs < 24) && (hrs > 0) && (mnts < 60) && (mnts > 0)) {
+            req.session.time = req.body.hrs + ":" + req.body.mnts;
+            req.session.serviceId = req.body.serviceId;
+            res.redirect('/services/'+req.body.serviceId)
+        }
+        else {
+            res.redirect("back");
+        }
     };
 }
