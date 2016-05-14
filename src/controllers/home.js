b24639569f05974e05dafedee093e280fb167cac
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-14 16:19:00 +0300

    don't show old orders

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -8,6 +8,23 @@ module.exports = function HomeController(req, res, next) {
             if (err) {
                 return next(err);
             }
+            var now = new Date();
+
+            var orders = orders.filter(function(order) {
+                var hrs = order.time.substring(0, order.time.indexOf(":"));
+                var mnts = order.time.substring(order.time.indexOf(":") + 1);
+                var date = new Date();
+                date.setHours(hrs);
+                date.setMinutes(mnts);
+
+                if (now.getTime() > date.getTime()){
+                    return 0;
+                }
+                return 1;
+            });
+
+            console.log(orders);
+
             function isSubscriber(subscribers){
                 return subscribers.some(function(subscriber){
                     return subscriber.person.id == req.user.id;
