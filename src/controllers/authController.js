ecbfe065d7f6404b8b6ae0d4930ef3afd1a76d6b
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-22 22:22:12 +0300

    delete views/firstScreen. Correct mistake authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,3 @@
-var OrderModel = require('../models/order').model;
-var OrderModel = require('../models/order').model;
-var PersonModel = require('../models/person').model;
-
 module.exports = function authController(req, res) {
 	res.render('authPage', {});
 }
