cd694a0dbcad2efd0bb222fe4825ef07a4ac3e1d
Evgeniy Zelenkov <skyydail@gmail.com>
2016-05-22 20:59:15 +0300

    some changes

--- src/controllers/setTime.js
+++ src/controllers/setTime.js
@@ -1,7 +1,6 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function setTime(req, res, next) {
-    if (req.body.action == 'setTime') {
         var hrs = +req.body.hrs;
         var mnts = +req.body.mnts;
         if (((hrs ^ 0) === hrs) && ((mnts ^ 0) === mnts) && (hrs < 24) && (hrs > 0) && (mnts < 60) && (mnts > 0)) {
@@ -13,4 +12,3 @@ module.exports = function setTime(req, res, next) {
             res.redirect("back");
         }
     };
-};
