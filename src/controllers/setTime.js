44d8fceee889d07c095e645112184dcdba09225b
Evgeniy Zelenkov <skyydail@gmail.com>
2016-05-22 21:19:24 +0300

    err in setTIme

--- src/controllers/setTime.js
+++ src/controllers/setTime.js
@@ -1,6 +1,7 @@
 var ServiceModel = require('../models/service').model;
 
 module.exports = function setTime(req, res, next) {
+    if (req.body.action == 'setTime') {
         var hrs = +req.body.hrs;
         var mnts = +req.body.mnts;
         if (((hrs ^ 0) === hrs) && ((mnts ^ 0) === mnts) && (hrs < 24) && (hrs > 0) && (mnts < 60) && (mnts > 0)) {
@@ -12,3 +13,4 @@ module.exports = function setTime(req, res, next) {
             res.redirect("back");
         }
     };
+};
